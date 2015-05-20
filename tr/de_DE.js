b934f482b9d08c90fd2bc196534f2cc6d3a0a941
xamblak <dev@xamblak.de>
2015-05-20 23:41:40 +0200

    Added gh-forkme and fixed some styling

--- tr/de_DE.js
+++ tr/de_DE.js
@@ -1,7 +1,7 @@
 // German translation
 R.registerLocale('de-DE', 
 {  
-  'title': "Smarte & Einfache lokale Übersetzung mit Javaskript",
+  'title': "Einfache lokale Übersetzung mit Javaskript",
   'subtitle': "Das ist eine test Implementierung (Wrapper) für die R.js lib",
   'test1': "Ich bin ein Text mit Nummer %i",
   'test2': "Ich bin äin weiterer Text mit Tüxt %s"
--- tr/en_GB.js
+++ tr/en_GB.js
@@ -1,7 +1,7 @@
 // English translation
 R.registerLocale('en-GB', 
 {
-  'title': "Smart & Easy local Translation using Java-Script",
+  'title': "Easy local Translation using Java-Script",
   'subtitle': "This is a test implementation (wrapper) for the R.js lib",
   'test1': "I am a string with a number %i",
   'test2': "I am another string with a string %s"
