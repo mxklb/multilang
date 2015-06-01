837f4743d2e98ddef60b8c0f0daf01698cb6d424
xamblak <dev@xamblak.de>
2015-06-01 22:21:39 +0200

    Added github link to the translation

--- tr/en_GB.js
+++ tr/en_GB.js
@@ -3,5 +3,6 @@ R.registerLocale('en-GB',
 {
   'subtitle': "This is an example demonstrating local HTML internationalisation with <a href='https://github.com/keithamus/R.js' target='_blank'>R.js</a>",
   'test1': "I am a string with a number %i",
-  'test2': "I am another string with a string %s"
+  'test2': "I am another string with a string %s",
+  'ghlink': "about"
 });
